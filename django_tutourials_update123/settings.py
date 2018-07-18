04eb0903c79b869cc8379349070aca78bfe2f962
James Lee <kidosol3616@gmail.com>
2018-07-18 02:43:39 +0000

    initial commit

--- django_tutourials_update123/settings.py
+++ django_tutourials_update123/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$6%@)h+2wi*aqut2a7acqn%%d!uxh6%xpwc%lxnb9z!un13imp'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=True, cast=bool)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
+ALLOWED_HOSTS = ['viber888.herokuapp.com']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
