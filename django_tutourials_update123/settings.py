1604b0c8614f4b039b8ba1fcbe24dedcbab07c96
James Lee <kidosol3616@gmail.com>
2018-07-18 12:39:32 +0000

    initial commit

--- django_tutourials_update123/settings.py
+++ django_tutourials_update123/settings.py
@@ -12,17 +12,17 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import dj_database_url
-DATABASES = {
-    'default': dj_database_url.config(
-        default=dj_database_url.config('DATABASE_URL')
-    )
-}
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SITE_ROOT = os.path.dirname(__file__)
 
-
+DATABASES = {
+    'default': dj_database_url.config(
+        default=dj_database_url.config('DATABASE_URL')
+    )
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
