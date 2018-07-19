5719718ae56dbb9c3f814002a7c31c24a92b126e
James Lee <kidosol3616@gmail.com>
2018-07-19 12:06:58 +0000

    initial commit

--- music/views.py
+++ music/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
 from django.views.generic import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 from .models import Album
 
 class IndexView(generic.ListView):
@@ -19,3 +20,10 @@ class AlbumCreate(CreateView):
     fields = ['artist' ,'album_title' , 'genre' , 'album_logo']
     
 #awesome
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist' ,'album_title' , 'genre' , 'album_logo']
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
